07e00fecb5ffc01f7cec4fb10213a03d4f1bd28b
fmiya <fmiya@10.202.146.210>
2020-09-03 12:42:03 +0900

    paiza

---
+++ src/paiza/Test.java
@@ -0,0 +1,33 @@
+package paiza;
+import java.util.Scanner;
+public class Test {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int section = sc.nextInt();
+			int speedLimit = sc.nextInt();
+			int[] roundSpeed = new int[section];
+			int[] roundSection = new int[section];
+			int outCheck = 0;
+			for(int i=0;i<section;i++){
+				roundSection[i]= sc.nextInt();
+				roundSpeed[i]= sc.nextInt();
+			}
+			for(int i=1;i<section;i++) {
+				try {
+					int sectionCheck = roundSection[i] - roundSection[i-1];
+					int speedCheck = roundSpeed[i]- roundSpeed[i-1];
+					if(speedLimit <= speedCheck / sectionCheck) {
+						System.out.println("YES");
+						outCheck++;
+						break;
+					}
+				} catch (ArrayIndexOutOfBoundsException e){
+				}
+			}
+
+			if(outCheck == 0) {
+				System.out.println("NO");
+			}
+		}
+	}
+}
---
+++ src/paiza/TimeDifference.java
@@ -0,0 +1,53 @@
+package paiza;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class TimeDifference {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int country = sc.nextInt();
+			Map<String, Integer> countryMap = new HashMap<>();
+			int[] timeDifference = new int[country];
+			for(int i=0;i<country;i++) {
+				String countryName = sc.next();
+				timeDifference[i]= sc.nextInt(); 
+			}
+			String localCountryName = sc.next();
+			String localTime = sc.next();
+			Calendar  localCountryTime = stringChangeDate(localTime);
+
+			for(int time : timeDifference) {
+				calcTimeDifference(localCountryTime,time);
+			}
+		}
+	}
+
+    public static Calendar stringChangeDate(String localTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+            Date date = null;
+			try {
+				date = sdf.parse(localTime);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+            Calendar localCountryTime = Calendar.getInstance();
+            localCountryTime.setTime(date);
+            return localCountryTime;
+    }
+
+    public static void calcTimeDifference(Calendar localCountryTime,int timeDifference) {
+    	localCountryTime.add(localCountryTime.HOUR, timeDifference);
+    	Date  calcTimeDifference = localCountryTime.getTime();
+    	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        System.out.println(sdf.format(calcTimeDifference));
+
+	}
+
+
+    }
